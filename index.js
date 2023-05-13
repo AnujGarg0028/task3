d8db928c8aff7754121887e0c6f93afa8fe508ab
AnujGarg0028 <132124027+AnujGarg0028@users.noreply.github.com>
2023-05-13 14:25:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
-
-const credential = new DefaultAzureCredential();
-
-console.log(credential);
-
-const keyvaultname='anujkeyvault';
-const url='http://'+keyvaultname+'.vault.azure.net';
-
-const client=new SecretClient(url,credential);
-
 app.get('/test', (req, res) => {
    res.send('Hello, World!');
 });
 
-app.get('/key', async (req, res) => {
-   const secret=await client.getSecret('pass');
-   res.send(secret.value);
-});
 app.listen(process.env.port || process.env.PORT || port, () => {console.log(`Server listening on port ${port}`);
 });
